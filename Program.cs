4999527730a78674e456cb5cc47c40bd6d10be80
Xian <kurt.goedel.1930@googlemail.com>
2021-02-13 23:01:50 +0100

    try to change the files

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace pipelines_dotnet_core
             CreateWebHostBuilder(args).Build().Run();
         }
         /* just try */
+        /* try again */
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
